6d27ef45f94d495964f080fb16c9b76be3a9b4b4
Drozd_AS <31595916+SD-SanaZI@users.noreply.github.com>
2026-01-19 00:13:11 +0300

    update input filter

--- feature/login/presentation/src/main/java/com/sanazi/login/presentation/LoginFragment.kt
+++ feature/login/presentation/src/main/java/com/sanazi/login/presentation/LoginFragment.kt
@@ -3,6 +3,7 @@ package com.sanazi.login.presentation
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -18,6 +19,7 @@ class LoginFragment : Fragment(R.layout.input) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         LoginComponent.create(getAppDependenciesProvider()).inject(this)
+        view.findViewById<EditText>(R.id.emailInput).inputType
         view.findViewById<Button>(R.id.button).setOnClickListener {
             viewModel.login(view, findNavController(), inputChecker)
         }
